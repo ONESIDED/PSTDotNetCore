cc00003eb7d37cde39779218a86f69907743777b
Paing Soe Thu <whoistheblack@gmail.com>
2024-11-12 19:03:00 +0700

    feat: ado.net create, edit, update

---
+++ DotNetTrainigBatch5.ConsoleApp/AdoDotNetExample.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetTrainigBatch5.ConsoleApp
+{
+    public class AdoDotNetExample
+    {
+        private readonly string _connectionString = "Data Source= MSI\\MSSQLSERVER2019; Initial Catalog=DotNetTrainingBatch5; User ID=sa; Password=sasa;";
+        public void Read()
+        {
+            Console.WriteLine("Connection String: " + _connectionString);
+            SqlConnection connection = new SqlConnection(_connectionString);
+
+            //Console.WriteLine("Connection Opening...");
+            connection.Open();
+            //Console.WriteLine("Connection Opened.");
+
+            string query = @"SELECT [BlogId]
+      ,[BlogTitle]
+      ,[BlogAuthor]
+      ,[BlogContent]
+      ,[DeleteFlag]
+  FROM [dbo].[Tbl_BLog] where Tbl_Blog.[DeleteFlag] = 0";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            reader.Read();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["BlogId"]);
+                Console.WriteLine(reader["BlogTitle"]);
+                Console.WriteLine(reader["BlogAuthor"]);
+                Console.WriteLine(reader["BlogContent"]);
+            }
+
+            //SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+            //DataTable dt = new DataTable();
+            //adapter.Fill(dt);
+
+            //foreach (DataRow dr in dt.Rows)
+            //{
+            //    Console.WriteLine(dr["BlogId"]);
+            //    Console.WriteLine(dr["BlogTitle"]);
+            //    Console.WriteLine(dr["BlogAuthor"]);
+            //    Console.WriteLine(dr["BlogContent"]);
+            //    //Console.WriteLine(dr["DeleteFlag"]);
+            //}
+
+            //Console.WriteLine("Connection Closing...");
+            connection.Close();
+            //Console.WriteLine("Connection Closed.");
+
+            // DataSet
+            // DataTable
+            // DataRow
+            // DataColumn
+
+
+            //foreach (DataRow dr in dt.Rows)
+            //{
+            //    Console.WriteLine(dr["BlogId"]);
+            //    Console.WriteLine(dr["BlogTitle"]);
+            //    Console.WriteLine(dr["BlogAuthor"]);
+            //    Console.WriteLine(dr["BlogContent"]);
+            //    //Console.WriteLine(dr["DeleteFlag"]);
+            //}
+
+        }
+
+        public void Create()
+        {
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+
+            Console.WriteLine("Please Blog Title: ");
+            string title = Console.ReadLine();
+
+            Console.WriteLine("Please Blog Author: ");
+            string author = Console.ReadLine();
+
+            Console.WriteLine("Please Blog Content: ");
+            string content = Console.ReadLine();
+
+            //string queryInsert = $@"INSERT INTO [dbo].[Tbl_BLog]
+            //           ([BlogTitle]
+            //           ,[BlogAuthor]
+            //           ,[BlogContent]
+            //           ,[DeleteFlag])
+            //     VALUES
+            //           ('{title}'
+            //           ,'{author}'
+            //           ,'{contect}'
+            //           ,'0')";
+
+            string query = $@"INSERT INTO [dbo].[Tbl_BLog]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent]
+           ,[DeleteFlag])
+     VALUES
+           (@BlogTitle
+           ,@BlogAuthor
+           ,@BlogContent
+           ,'0')";
+
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            //SqlDataAdapter adapter = new SqlDataAdapter(cmd2);
+            //DataTable dt = new DataTable();
+            //adapter.Fill(dt);
+
+            if (string.IsNullOrEmpty(title))
+            {
+                return;
+            }
+
+            if (string.IsNullOrEmpty(author))
+            {
+                return;
+            }
+
+            if (string.IsNullOrEmpty(content))
+            {
+                return;
+            }
+            cmd.Parameters.AddWithValue("@BlogTitle", title);
+            cmd.Parameters.AddWithValue("@BlogAuthor", author);
+            cmd.Parameters.AddWithValue("@BlogContent", content);
+
+            int result = cmd.ExecuteNonQuery();
+
+            Console.WriteLine(result == 1 ? "Insert Successful." : "Inesert Failed.");
+
+            connection.Close();
+        }
+
+        public void Edit()
+        {
+            //Console.WriteLine("Blog Id: ");
+            //Console.WriteLine(); နဲရေးမယ်ဆိုရင် ကိုယ်ရေးတဲံ့စာကို အောက်တစ်ကြောင်းဆင်းပြီးပြနေပါလိမ့်မယ်
+            Console.Write("Blog Id: ");
+            string id = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(id)) { return; }
+
+            SqlConnection connection = new SqlConnection(_connectionString);
+
+            connection.Open();
+
+            string query = $@"SELECT [BlogId]
+      ,[BlogTitle]
+      ,[BlogAuthor]
+      ,[BlogContent]
+      ,[DeleteFlag]
+  FROM [dbo].[Tbl_BLog] where BlogId = @BlogId and [DeleteFlag] = 0";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogId", id);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+            DataTable dt = new DataTable();
+
+            adapter.Fill(dt);
+
+            connection.Close(); // data ကိုယူတဲ့ ကိစ္စပြီးတာနဲ connection ပိတ်တာကကောင်းပါတယ်။
+
+            if (dt.Rows.Count is 0) // row ရဲ့ count က ၀ နဲညီနေရင် data မပါလာလို့ပါ။
+            {
+                Console.WriteLine("No data found...");
+                return;
+            }
+
+            DataRow dr = dt.Rows[0];
+            Console.WriteLine(dr["BlogId"]);
+            Console.WriteLine(dr["BlogTitle"]);
+            Console.WriteLine(dr["BlogAuthor"]);
+            Console.WriteLine(dr["BlogContent"]);
+
+            #region လုပ်ခဲ့တာအဆင့်ဆင့်
+
+            // ၁ id တောင်းတယ် သူကမပေးခဲ့ရင် ရပ်ရမယ်။
+            // ၂ connection ကိုဆောက်တယ် ပြီးရင် connection ဖွင့်ခဲ့ရမယ်
+            // ၃ query ရေးရမယ်
+            // ၄ cmd ကိုဆောက်ရမယ် သူ့ထဲကို query နဲ con ကိုထည့်ရမယ်
+            // ၅ i/o operation ကိုလုပ်ရမယ် 
+            // adapter ကိုသုံးရင် adapter ကိုဆောက် (or) reader ကို ဖတ်မယ်ဆို reader ကိုဆောက်ရမယ်
+            // ၆ data ကိုယူတဲ့ ကိစ္စပြီးတာနဲ connection ပိတ်တာက အကောင်းဆုံးပါပဲ။
+            // ၇ row ရဲ့ count က ၀ နဲညီနေရင် data မပါလာလို့ပါ။ သူ့အတွက်စစ်ပေးရမယ်။
+
+            #endregion
+        }
+
+        public void Update() 
+        {
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+
+            Console.WriteLine("Please Blog ID: ");
+            string id = Console.ReadLine();
+
+            Console.WriteLine("Please Blog Title: ");
+            string title = Console.ReadLine();
+
+            Console.WriteLine("Please Blog Author: ");
+            string author = Console.ReadLine();
+
+            Console.WriteLine("Please Blog Content: ");
+            string content = Console.ReadLine();
+
+            string query = @"UPDATE [dbo].[Tbl_BLog]
+   SET [BlogTitle] = @BlogTitle
+      ,[BlogAuthor] = @BlogAuthor
+      ,[BlogContent] = @BlogContent
+      ,[DeleteFlag] = 0
+ WHERE [BlogId] = @BlogId and [DeleteFlag] != 1";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue ("@BlogId", id);
+            cmd.Parameters.AddWithValue("@BlogTitle", title);
+            cmd.Parameters.AddWithValue("@BlogAuthor", author);
+            cmd.Parameters.AddWithValue("@BlogContent", content);
+
+            int result = cmd.ExecuteNonQuery();
+
+            connection.Close();
+
+            Console.WriteLine(result == 1 ? "Update Successful." : "Update Failed.");
+        }
+    }
+}
--- DotNetTrainigBatch5.ConsoleApp/Program.cs
+++ DotNetTrainigBatch5.ConsoleApp/Program.cs
@@ -1,15 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using DotNetTrainigBatch5.ConsoleApp;
 using System.ComponentModel.DataAnnotations;
 using System.Data;
 using System.Data.SqlClient;
 
 
-const string connectionString = "Data Source= MSI\\MSSQLSERVER2019; Initial Catalog=DotNetTrainingBatch5; User ID=sa; Password=sasa;";
-
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 //Console.Read();
-Console.ReadKey();
+//Console.ReadKey();
 
 #region
 //Console.ReadLine();
@@ -32,121 +31,24 @@ Console.ReadKey();
 // 100 = 99
 
 //101
-#endregion
 
-Console.WriteLine("Connection String: "+ connectionString);
-SqlConnection connection = new SqlConnection(connectionString);
-
-Console.WriteLine("Connection Opening...");
-connection.Open();
-Console.WriteLine("Connection Opened.");
-
-string query = @"SELECT [BlogId]
-      ,[BlogTitle]
-      ,[BlogAuthor]
-      ,[BlogContent]
-      ,[DeleteFlag]
-  FROM [dbo].[Tbl_BLog] where Tbl_Blog.[DeleteFlag] = 0";
-
-SqlCommand cmd = new SqlCommand(query, connection);
-
-SqlDataReader reader = cmd.ExecuteReader();
-
-reader.Read();
-while (reader.Read())
-{
-    Console.WriteLine(reader["BlogId"]);
-    Console.WriteLine(reader["BlogTitle"]);
-    Console.WriteLine(reader["BlogAuthor"]);
-    Console.WriteLine(reader["BlogContent"]);
-}
-
-// ExecuteReaderAsnyc အကြောင်းကိုမေးရန်
-// dr နေရာမှာ reader ကိုပဲရွေးပြီး select မှတ်တာကိုမေးရန် ?1
+// ExecuteReaderAsnyc အကြောင်းကိုမေးရန် ျ await သိရန်လိုတယ် 
+// dr နေရာမှာ reader ကိုပဲရွေးပြီး select မှတ်တာကိုမေးရန် ?1 ိdone (Alt + left click drag)
 // မှားပြီး commit all လုပ်လိုက်တာကို push မလုပ်ခင် message ပြန်ပြင်ချင်ရင် ဘယ်လိုပြန်လုပ်လို့ရလဲ နာမည်ပြန်ခွဲပြီးပေးချင်တာမျိုးပါ ?2
 
-//SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-
-//DataTable dt = new DataTable();
-//adapter.Fill(dt);
-
-//foreach (DataRow dr in dt.Rows)
-//{
-//    Console.WriteLine(dr["BlogId"]);
-//    Console.WriteLine(dr["BlogTitle"]);
-//    Console.WriteLine(dr["BlogAuthor"]);
-//    Console.WriteLine(dr["BlogContent"]);
-//    //Console.WriteLine(dr["DeleteFlag"]);
-//}
-
-Console.WriteLine("Connection Closing...");
-connection.Close();
-Console.WriteLine("Connection Closed.");
-
-// DataSet
-// DataTable
-// DataRow
-// DataColumn
+#endregion
 
 
-//foreach (DataRow dr in dt.Rows)
-//{
-//    Console.WriteLine(dr["BlogId"]);
-//    Console.WriteLine(dr["BlogTitle"]);
-//    Console.WriteLine(dr["BlogAuthor"]);
-//    Console.WriteLine(dr["BlogContent"]);
-//    //Console.WriteLine(dr["DeleteFlag"]);
-//}
-
-connection.Open();
-
-Console.WriteLine("Please Blog Title: ");
-string title = Console.ReadLine();
-
-Console.WriteLine("Please Blog Author: ");
-string author = Console.ReadLine();
-
-Console.WriteLine("Please Blog Content: ");
-string contect = Console.ReadLine();
-
-//string queryInsert = $@"INSERT INTO [dbo].[Tbl_BLog]
-//           ([BlogTitle]
-//           ,[BlogAuthor]
-//           ,[BlogContent]
-//           ,[DeleteFlag])
-//     VALUES
-//           ('{title}'
-//           ,'{author}'
-//           ,'{contect}'
-//           ,'0')";
-
-string queryInsert = $@"INSERT INTO [dbo].[Tbl_BLog]
-           ([BlogTitle]
-           ,[BlogAuthor]
-           ,[BlogContent]
-           ,[DeleteFlag])
-     VALUES
-           (@BlogTitle
-           ,@BlogAuthor
-           ,@BlogContent
-           ,'0')";
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//adoDotNetExample.Read();
+//adoDotNetExample.Create();
+//adoDotNetExample.Edit();
+adoDotNetExample.Update();
 
 
-SqlCommand cmd2 = new SqlCommand(queryInsert, connection);
-//SqlDataAdapter adapter = new SqlDataAdapter(cmd2);
-//DataTable dt = new DataTable();
-//adapter.Fill(dt);
-
-cmd2.Parameters.AddWithValue("@BlogTitle", title);
-cmd2.Parameters.AddWithValue("@BlogAuthor", title);
-cmd2.Parameters.AddWithValue("@BlogContent", title);
-
-int result = cmd2.ExecuteNonQuery();
-
-Console.WriteLine(result == 1 ? "Insert Successful." : "Inesert Failed.");
-
-connection.Close();
 
 Console.ReadKey();
+
+
 
 
