5c79ad9c324f6aa03e7a451f725b4bfe1ec08152
Paing Soe Thu <whoistheblack@gmail.com>
2024-11-27 02:26:11 +0700

    feat: null value warning cleaned (required to check back)

--- DotNetTrainigBatch5.ConsoleApp/Models/BlogDataModel.cs
+++ DotNetTrainigBatch5.ConsoleApp/Models/BlogDataModel.cs
@@ -9,13 +9,11 @@ namespace DotNetTrainigBatch5.ConsoleApp.Models
     public class BlogDataModel
     {
         public int BlogId { get; set; }
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set; }
-        public string BlogContent { get; set; }
+        public string? BlogTitle { get; set; }
+        public string? BlogAuthor { get; set; }
+        public string? BlogContent { get; set; }
         public bool DeleteFlag { get; set; }
 
         // 1
-
-
     }
 }
